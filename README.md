d49ad5af508a5afc7bf6ea51f49d7c87dc881536
Awilum <awilum@msn.com>
2015-11-25 18:01:59 +0300

    Init Commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# morfy-plugin-page-inject
+# Page Inject
 Page Inject plugin allows you to inject the page into other pages.
+
+## Installation
+See [this instruction](http://morfy.org/documentation/plugins/plugins-installation)
+
+## Usage
+
+Simple shortcode `page` with parameter `name`
+
+```
+{page name=contact}
+```
+
+Also you can set a specific page `field` to return
+
+```
+{page name=contact field=title}
+```
+
+## License
+See [LICENSE](https://github.com/morfy-cms/morfy-plugin-page-inject/blob/master/LICENSE)
---
+++ page-inject.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Morfy Page Inject Plugin
+ *
+ * (c) Romanenko Sergey / Awilum <awilum@msn.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+Shortcode::add('page', function ($attributes) {
+
+    // Extract
+    extract($attributes);
+
+    $page = [];
+    $data = '';
+
+    if (isset($name)) {
+        $page = Pages::getPage($name);
+    }
+
+    if (isset($field)) {
+        $data = isset($page[$field]) ? $page[$field] : 'Field '.$field.' not found';
+    } else {
+        $data = $page['content'];
+    }
+
+    return $data;
+});
